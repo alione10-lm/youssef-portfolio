a272756d059bfee7625ab78be42980d7aa958629
ali1 <aliouaneoussama89@gmail.com>
2025-01-04 22:50:08 +0100

    add some styles

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -71,11 +71,20 @@ function Contact() {
                   type="text"
                   name="floating_first_name"
                   id="floating_first_name"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  className="block transition-all  duration-200 px-2 md:px-5 py-3 rounded-lg w-full border-none bg-slate-100 focus:ring focus:ring-blue-700 focus:outline-none focus:ring-offset-2 text-slate-800"
                   placeholder="First name"
                   required
                   {...register("firstName")}
                 />
+                {/* <input
+                  type="text"
+                  name="floating_first_name"
+                  id="floating_first_name"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder="First name"
+                  required
+                  {...register("firstName")}
+                /> */}
                 {/* <label
                   htmlFor="floating_first_name"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
@@ -88,7 +97,8 @@ function Contact() {
                   type="text"
                   name="floating_last_name"
                   id="floating_last_name"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  // className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  className="block transition-all w-full duration-200 px-5 py-3 rounded-lg border-none bg-slate-100 focus:ring focus:ring-blue-700 focus:outline-none focus:ring-offset-2 text-slate-800"
                   placeholder="Last name "
                   required
                   {...register("lastName")}
@@ -106,7 +116,7 @@ function Contact() {
                 type="email"
                 name="floating_email"
                 id="floating_email"
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                className="block transition-all w-full duration-200 px-5 py-3 rounded-lg border-none bg-slate-100 focus:ring focus:ring-blue-700 focus:outline-none focus:ring-offset-2 text-slate-800"
                 placeholder="Email address"
                 required
                 {...register("email")}
@@ -121,7 +131,7 @@ function Contact() {
 
             <div className="relative z-0 w-full mb-5 group">
               <textarea
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                className="block transition-all w-full duration-200 px-5 py-3 rounded-lg border-none bg-slate-100 focus:ring focus:ring-blue-700 focus:outline-none focus:ring-offset-2 text-slate-800"
                 required
                 placeholder="Message"
                 {...register("message")}
